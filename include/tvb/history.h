7b045f54fa473db344cff61b8782caf67c714fb8
Marmaduke Woodman <mmwoodman@gmail.com>
2017-01-14 00:51:17 +0100

    begin history buffer work

--- include/tvb/history.h
+++ include/tvb/history.h
@@ -21,15 +21,72 @@
 #define TVB_history
 
 #include <vector>
+#include <cmath>
 
 namespace tvb {
 
-    template <typename T> class ring_buffer
+    template <typename I> I wrap(I idx, I len) { return idx % len + (idx < 0) * len; };
+
+    template <typename value_type> class ring_buffer
     {
     public:
-        DRing(size_t size) buf(size) { }
+        using value_type = typename value_type;
+        DRing(size_t size) : buf(size) { }
+        template <typename I> T& operator[](const I idx) { return buf[wrap(idx, buf.size())]; }
+        size_t size() { return buf.size(); }
     private:
-        std::vector<T> buf;
+        std::vector<value_type> buf;
+    };
+
+    template <typename container_type, typename query_type> class interp_zero/*{{{*/
+    {
+    public:
+        using value_type = typename container_type::value_type;
+        using query_type = typename query_type;
+        interp_zero(query_type step) : _step(step) { }
+        value_type operator()(container_type container, query_type query) { return container[query / _step]; }
+    private:
+        const query_type _step;
+    };/*}}}*/
+
+    template <typename container_type, typename query_type> class interp_round/*{{{*/
+    {
+    public:
+        using value_type = typename container_type::value_type;
+        using query_type = typename query_type;
+        interp_zero(query_type step) : _step(step) { }
+        value_type operator()(container_type container, query_type query) { return container[round(query / _step)]; }
+    private:
+        const query_type _step;
+    };/*}}}*/
+
+    template <typename container_type, typename query_type> class interp_linear/*{{{*/
+    {
+    public:
+        using value_type = typename container_type::value_type;
+        using query_type = typename query_type;
+        interp_zero(query_type step) : _step(step) { }
+        value_type operator()(container_type& container, query_type query)
+        {
+            size_t index  = query / _step;
+            value_type left = container[index];
+            value_type right = container[index + 1];
+            value_type slope = (right - left) / _step;
+            value_type dquery = query % _step;
+            return slope * dquery + left;
+        }
+    private:
+        const query_type _step;
+    };/*}}}*/
+
+    void test()
+    {
+        using rb_t = ring_buffer<float>;
+        using interp_t = interp_linear<rb, float>;
+        
+        rb_t rb(32);
+        interp_t interp(0.1f);
+        interp(rb, 234.024);
     }
 
     template <typename Tx=float, typename Ty=Tx> class CRing // {{{
