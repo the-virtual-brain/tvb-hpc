a281bad5905da4710314d657943cc145b7d748d4
Marmaduke Woodman <mmwoodman@gmail.com>
2017-02-18 10:40:30 +0100

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='tvb-hpc',
+    version='0.0',
+    description='HPC code generation for TVB',
+    author='TVB-HPC Contributors',
+    url='https://github.com/the-virtual-brain/tvb-hpc',
+    packages=setuptools.find_packages(),
+)
