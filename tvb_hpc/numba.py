0b2cb178d415b5943eac2a82f2cd081a206ba43c
Marmaduke Woodman <mmwoodman@gmail.com>
2017-06-30 17:25:55 +0200

    add numba cuda executor

--- tvb_hpc/numba.py
+++ tvb_hpc/numba.py
@@ -3,7 +3,7 @@ import loopy as lp
 import loopy.target.numba as base_numba
 
 
-LOG = logging.getLogger(__name__)
+LOG = logging.getLogger('tvb_hpc')
 
 
 class NumbaTarget(base_numba.NumbaTarget):
@@ -17,3 +17,21 @@ class NumbaTarget(base_numba.NumbaTarget):
         ns = {}
         exec(code, ns)
         return ns[knl.name]
+
+
+class NumbaCudaTarget(base_numba.NumbaCudaTarget):
+
+    def get_kernel_executor_cache_key(self, *args, **kwargs):
+        return 'default'
+
+    def get_kernel_executor(self, knl, *args, **kwargs):
+        code, _ = lp.generate_code(knl)
+        LOG.debug(code)
+        ns = {}
+        exec(code, ns)
+        return self._wrap_dims(ns[knl.name])
+
+    def _wrap_dims(self, nbc_knl):
+        def _(griddim, blockdim, *args, **kwargs):
+            return nbc_knl[griddim, blockdim](*args, **kwargs)
+        return _
