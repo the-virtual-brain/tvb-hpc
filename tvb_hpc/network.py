08edf6b43a6f092ebd4d5a25b0708b97c5a4dda3
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2018-02-10 22:07:37 +0100

    happy new year

--- tvb_hpc/network.py
+++ tvb_hpc/network.py
@@ -1,4 +1,4 @@
-#     Copyright 2017 TVB-HPC contributors
+#     Copyright 2018 TVB-HPC contributors
 #
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
